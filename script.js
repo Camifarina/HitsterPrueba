5abe098448af9b6d898b74d4d360f6e670fe3d28
camila <camilafarina03@gmail.com>
2025-06-02 18:19:08 -0300

    dc

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+/* import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -9,7 +9,7 @@ const firebaseConfig = {
   
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
-  
+   */
 
 function mostrarReglas() {
     document.getElementById("menu").style.display = "none";
